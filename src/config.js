ca0d4dc7dafc57169d9f877750d5d0588a158ce3
Jonas <git@jonasscheiwiller.ch>
2023-02-21 15:06:58 +0100

    feat(config): update urls

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 export default {
-  recordingsUrl: 'https://zzz.megahex.fm/recordings/',
-  uploadsUrl: 'https://zzz.megahex.fm/uploads/upstate/',
-  streamUrl: 'https://zzz.megahex.fm:8001/listen?type=.mp3',
-  apiBaseUrlLocal: 'http://cms.megahex.test/proxy', // using a proxy to auth with kirby
+  recordingsUrl: "https://kein.fm/megahex-json/",
+  uploadsUrl: "https://kein.fm/megahex-json/uploads/upstate/",
+  streamUrl: "https://zzz.megahex.fm:8001/listen?type=.mp3",
+  apiBaseUrlLocal: "http://cms.megahex.test/proxy", // using a proxy to auth with kirby
   // apiBaseUrlLocal: 'https://cms.megahex.fm/proxy', // using a proxy to auth with kirby
-  apiBaseUrlRemote: 'https://cms.megahex.fm/proxy', // using a proxy to auth with kirby
-  imageBaseUrlRemote: 'https://cms.megahex.fm/media',
-  imageBaseUrlLocal: 'http://cms.megahex.test/media',
-  homeSlug: 'home'
-}
+  apiBaseUrlRemote: "https://cms.megahex.fm/proxy", // using a proxy to auth with kirby
+  imageBaseUrlRemote: "https://cms.megahex.fm/media",
+  imageBaseUrlLocal: "http://cms.megahex.test/media",
+  homeSlug: "home",
+};
